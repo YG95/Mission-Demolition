9be7f06e770692e9ed97ffb876e383668692a001
YG95 <yang.3.guo@gmail.com>
2016-10-13 01:41:05 -0400

    Created Castles and finished Mission Demolition!

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -68,6 +68,7 @@ public class Slingshot : MonoBehaviour {
 			projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
 			FollowCam.S.poi = projectile;
 			projectile = null;
+			MissionDemolition.ShotFired();
 		}
 	}
 }
