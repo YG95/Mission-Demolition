b18f3202d3e4ffecfa8075ec8b73cb112c4271e7
YG95 <yang.3.guo@gmail.com>
2016-10-12 23:30:39 -0400

    Fixed some errors to slingshot positioning

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -2,12 +2,20 @@
 using System.Collections;
 
 public class Slingshot : MonoBehaviour {
+	// fields set in the Unity Inspector pane
+	public GameObject prefabProjectile;
+	public bool _____________________________;
+	// fields set dynamically
 	public GameObject	launchPoint; 
+	public Vector3 launchPos;
+	public GameObject projectile;
+	public bool aimingMode;
 
 	void Awake() {
 		Transform launchPointTrans = transform.Find("LaunchPoint");
 		launchPoint = launchPointTrans.gameObject;
 		launchPoint.SetActive( false );
+		launchPos = launchPointTrans.position;
 	}
 
 	void OnMouseEnter() {
@@ -16,6 +24,17 @@ public class Slingshot : MonoBehaviour {
 	}
 	void OnMouseExit() {
 		//print("Slingshot:OnMouseExit()");
-		launchPoint.SetActive( false );
+		launchPoint.SetActive (false);
+	}
+
+	void OnMouseDown() {
+		// The player has pressed the mouse button while over Slingshot
+		aimingMode = true;
+		// Instantiate a Projectile
+		projectile = Instantiate( prefabProjectile ) as GameObject;
+		// Start it at the launchPoint
+		projectile.transform.position = launchPos;
+		// Set it to isKinematic for now
+		projectile.GetComponent<Rigidbody>().isKinematic = true;
 	}
 }
